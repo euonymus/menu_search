620ce4f936c05a71da6ad1668bc398e069db6390
euonymus <euonymus0220@gmail.com>
2014-12-02 13:44:06 +0900

    debug level

--- app/Config/core.php
+++ app/Config/core.php
@@ -32,7 +32,8 @@
  * In development mode, you need to click the flash message to continue.
  */
         //Configure::write('debug', 2);
-	Configure::write('debug', 1);
+        //Configure::write('debug', 1);
+        config('debuglevel');
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
